1e2508a1dae9bf6f7fd4ea47f954b05f9b748572
AsAlmas <asalmas0005@gmail.com>
2025-10-31 23:02:29 +0300

    Sign handler and sign-in/sign-up system

---
+++ ECB v2.0.0/Registration/HandleSign.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECB_v2.Registration
+{
+    internal class HandleSign
+    {
+        private string _name;
+        private string _job;
+        private enum RING { 
+            OWNER,
+            SELLER
+        };
+        public HandleSign() { }
+
+    }
+}
---
+++ ECB v2.0.0/Session.cs
@@ -0,0 +1,15 @@
+﻿using ECB_v2.Scanner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECB_v2
+{
+    internal static class Session
+    {
+        public static ScannerHandle scanner = null;
+        public static Registration.HandleSign Auth = null;
+    }
+}
